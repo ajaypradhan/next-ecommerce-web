de70986924dd714dad92ded904ab1adec0d332ee
Ajay Pradhan <pradhanajay14@gmail.com>
2023-10-02 00:27:18 +0530

    added to cart done

--- src/app/components/Product.js
+++ src/app/components/Product.js
@@ -1,7 +1,15 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../../redux/CartReducer";
 
 function Product({ product }) {
+  const dispatch = useDispatch();
+  const [added, setAdded] = useState(false);
+  const addProductToCart = (product) => {
+    setAdded(true);
+    dispatch(addToCart(product));
+  };
   return (
     <div className="relative flex flex-col border bg-white m-5 p-3">
       <p>{product?.category}</p>
@@ -13,8 +21,11 @@ function Product({ product }) {
       </h4>
       <p className="text-sm line-clamp-2 mt-1">{product?.description}</p>
       <h3 className="text-md font-semibold mt-1">₹ {product?.price}</h3>
-      <button className="p-2 mt-2 text-sm md:text-sm bg-gradient-to-b from-yellow-200 to-yellow-400">
-        Add to Cart
+      <button
+        onClick={() => addProductToCart(product)}
+        className="p-2 mt-2 text-sm md:text-sm bg-gradient-to-b from-yellow-200 to-yellow-400"
+      >
+        {added ? "Added to Cart" : "Add to Cart"}
       </button>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+"use client";
+import { useSelector } from "react-redux";
 import Deals from "./components/Deals";
 import Header from "./components/Header";
 import HeroBanner from "./components/HeroBanner";
 import ProductFeed from "./components/ProductFeed";
 
 export default function Home() {
+  const cart = useSelector((state) => state.cart.cart);
+  // console.log(cart);
   return (
     <main className="">
       <Header />
