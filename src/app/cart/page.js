32a01ebe5b23562140b439cdd7b6ab3b41c0a46d
Ajay Pradhan <pradhanajay14@gmail.com>
2023-10-02 12:07:27 +0530

    cart right side ui done

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -12,12 +12,15 @@ function Cart() {
   return (
     <div>
       <Header />
-      <div className="relative grid grid-cols-8  h-screen bg-gray-200-200">
+      <div className="relative grid grid-cols-8  h-screen bg-gray-200">
         {/* left part */}
         <div className="flex flex-col col-span-5  mt-16 ml-20">
           <h2 className="text-xl font-bold ">Your Shopping Cart</h2>
           {cart?.map((item, index) => (
-            <div key={index} className="border rounded-md p-3 bg-white shadow-md flex flex-row space-x-4 my-4">
+            <div
+              key={index}
+              className="border rounded-md p-3 bg-white shadow-md flex flex-row space-x-4 my-4"
+            >
               <Image
                 width={80}
                 height={80}
@@ -58,13 +61,19 @@ function Cart() {
 
                 <div className="flex items-center space-x-2 mt-1">
                   <button className="rounded-full bg-gray-500">
-                    <MinusSmallIcon className="text-white h-4 w-4"  class="h-6 w-6 text-gray-500" />
+                    <MinusSmallIcon
+                      className="text-white h-4 w-4"
+                      class="h-6 w-6 text-white"
+                    />
                   </button>
 
                   <span className="text-gray-500">{item?.quantity}</span>
 
                   <button className="rounded-full bg-gray-500">
-                    <PlusSmallIcon  className="text-white h-4 w-4" class="h-6 w-6 text-gray-500" />
+                    <PlusSmallIcon
+                      className="text-white h-4 w-4"
+                      class="h-6 w-6 text-white"
+                    />
                   </button>
                 </div>
               </div>
@@ -72,7 +81,48 @@ function Cart() {
           ))}
         </div>
         {/* right part */}
-        <div className="col-span-2"></div>
+        <div className="col-span-2 mt-16 ml-20">
+          <h1 className="mb-2">Choose a delivery address</h1>
+          <div>
+            <div className="bg-white p-4 rounded-sm mt-2 ">
+              <h4 className="text-xs text-gray-400">Select Location</h4>
+              <p className="text-xs text-gray-400">
+                Please select location, so that we can find a place that
+                delivers to you
+              </p>
+              <button className="rounded-sm py-1 mt-2 text-sm font-semibold text-gray-600">
+                Add Address
+              </button>
+            </div>
+          </div>
+          <h3 className="mt-4">Offers</h3>
+          <div>
+            <div className="bg-white rounded-sm p-4 mt-2">
+              <h4 className="text-xs text-gray-400">Apply Coupon</h4>
+              <p className="text-xs text-gray-400">
+                Get Discount with you order
+              </p>
+            </div>
+          </div>
+
+          <h3 className="mt-1">Price Details</h3>
+          <div className="bg-white rounded-sm mt-2 p-3 space-y-3">
+            <div className="flex items-center justify-between">
+              <h4 className="text-xs ">Sub Total</h4>
+              <p className="text-sm font-normal">₹ 500</p>
+            </div>
+            <div className="flex items-center justify-between">
+              <h4 className="text-xs ">Discount</h4>
+              <p className="text-sm font-normal">₹ -</p>
+            </div>
+            <div className="flex items-center justify-between">
+              <h4 className="text-xs ">taxes and Charges</h4>
+              <p className="text-sm font-normal">₹ 65</p>
+            </div>
+            <hr className="w-full mt-1 decoration-dotted" />
+            <button className="bg-yellow-500 text-center font-normal rounded-md text-white py-2 px-3 w-full">Place Order</button>
+          </div>
+        </div>
       </div>
     </div>
   );
